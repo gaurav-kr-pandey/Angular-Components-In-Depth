7ae5163abae0f4f45c81469741fca31e66f1fe93
Gaurav Kumar Pandey <gaurav17p@gmail.com>
2019-08-26 19:35:11 +0530

    Play around with directives | Mouseenter | Mouseleave

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -10,11 +10,11 @@ export class HighlightDirective {
   
 
   @HostListener('mouseenter') addElement(){
-    this.element.nativeElement.style.backgroundColor=this.color;
+    this.element.nativeElement.style.fontSize='35px';
   }
 
   @HostListener('mouseleave') removeElement(){
-    this.element.nativeElement.style.backgroundColor=null;
+    this.element.nativeElement.style.fontSize=null;
   }
 
 }
